3e47c9d091dee6a9e7cf6320bad02a2c219075ff
PC 2 <>
2024-06-13 20:27:01 -0300

    update y delete

--- comandos.sql
+++ comandos.sql
@@ -9,18 +9,17 @@ USE primer_db;
 
 /* Crear una tabla*/
 CREATE TABLE productos(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    precio FLOAT DEFAULT 9999999,
-    nombre VARCHAR (20)
+    id INT PRIMARY KEY AUTO_INCREMENT, /* primary key nos indica q es algo importante*/
+    precio FLOAT DEFAULT 9999999, /* hay q pensar bien el nro de defecto*/
+    nombre VARCHAR (20) /*(20)longitud de caracteres limite*/
 );
-INSERT INTO productos (precio, nombre) VALUES (500000.123, "notebook");
+INSERT INTO productos (precio, nombre) VALUES (500000.123, "notebook"); /* INSERT INTO= Insertar en*/
 INSERT INTO productos (precio, nombre) VALUES (0.230, "notebook ojala");
 
 INSERT INTO productos () VALUES ();
 INSERT INTO productos (nombre) VALUES ( "algo");
 
 /* -------------------------- FIN CREATE ---------------------*/
-
 
 /* -------------------------- READ ---------------------*/
 /* Muestra las bases de datos existentes*/
@@ -33,11 +32,53 @@ SHOW TABLES;
 SELECT * FROM productos;
 
 /* -------------------------- FIN READ ---------------------*/
+
 /* -------------------------- UPDATE ---------------------*/
 /* para agregar*/
-ALTER TABLE productos ADD COLUMN tiene_oferta TINYINT (1) DEFAULT 0;
+ALTER TABLE productos ADD COLUMN tiene_oferta TINYINT (1) DEFAULT 0; /*ALTER TABLE= Modificar tabla. ADD= a. TINYINT =es para q sea 0 o 1. es como usar Boolaneos. DEFAULT 0= es 0 para q sea falso. xq todo nro Distinto a 0 es True */
 ALTER TABLE productos ADD COLUMN peso FLOAT;
-ALTER TABLE productos ADD COLUMN tamanio FLOAT NOT NULL;
+ALTER TABLE productos ADD COLUMN tamanio FLOAT NOT NULL;/*No puede ser Null*/
 
+ALTER TABLE productos ADD COLUMN unidad VARCHAR(3) NOT NULL; /*No puede ser Null en este caso como es VARCHAR crea un Vacio que seria 0 y lo convierte en caracter vacio= "" que es False*/
+
+UPDATE productos
+SET peso= 2 WHERE id =1; /*Set= definir. El WHERE es muy importante, xq sino se agrega en todos los productos*/
+
+UPDATE productos
+SET peso= 4 WHERE id =2;
+
+UPDATE productos
+SET unidad= "KG" WHERE id =1;
 
 /* -------------------------- FIN UPDATE ---------------------*/
+
+/* -------------------------- DELETE ---------------------*/
+DELETE FROM productos WHERE id= 3;
+DELETE FROM productos WHERE id= 2; /* Sino ponemos WHERE se borra todo por completo para Siempre */
+
+/* -------------------------- FIN DELETE ---------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+CREATE TABLE electrodomesticos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    precio FLOAT DEFAULT 9999999,
+    nombre VARCHAR (20)
+);
+INSERT INTO electrodomesticos (precio, nombre) VALUES (500000.123, "notebook");
+INSERT INTO electrodomesticos (precio, nombre) VALUES (0.230, "jarra ojala");
+INSERT INTO electrodomesticos () VALUES ();
+INSERT INTO electrodomesticos (nombre) VALUES ( "licuadora");
+
+ALTER TABLE electrodomesticos ADD COLUMN tiene_oferta TINYINT (1) DEFAULT 0;
+ALTER TABLE electrodomesticos ADD COLUMN peso FLOAT;
+ALTER TABLE electrodomesticos ADD COLUMN tamanio FLOAT NOT NULL;
--- db_base.sql
+++ db_base.sql
@@ -1,2 +1,17 @@
 /* El conjunto de comandos para reiniciar la base de datos */
 CREATE DATABASE primer_db;
+USE primer_db;
+CREATE TABLE productos(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    precio FLOAT DEFAULT 9999999, 
+    nombre VARCHAR (20),
+    tiene_oferta TINYINT (1) DEFAULT 0,
+    peso FLOAT,
+    unidad VARCHAR(3) NOT NULL,
+    tamanio FLOAT NOT NULL
+    );
+INSERT INTO productos (precio, nombre,unidad,tamanio) VALUES (500000.123, "notebook","KG",500);
+INSERT INTO productos (precio, nombre,unidad,tamanio) VALUES (0.230, "notebook ojala","KG",500);
+INSERT INTO productos (unidad,tamanio) VALUES ("KG",500);
+INSERT INTO productos (nombre,unidad,tamanio) VALUES ( "algo","KG",500);
+SELECT * FROM productos;
